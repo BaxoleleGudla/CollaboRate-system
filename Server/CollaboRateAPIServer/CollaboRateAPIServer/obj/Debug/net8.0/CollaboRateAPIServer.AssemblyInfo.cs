04c62988a5eeb047bcb747ff4463af5e423e53dc
BaxoleleGudla <baxolelegudla@gmail.com>
2026-09-05 16:04:29 +0200

    Added modifications

--- Server/CollaboRateAPIServer/CollaboRateAPIServer/obj/Debug/net8.0/CollaboRateAPIServer.AssemblyInfo.cs
+++ Server/CollaboRateAPIServer/CollaboRateAPIServer/obj/Debug/net8.0/CollaboRateAPIServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22226d0a15e3bbc5916909cb92d7100eb1b6512e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

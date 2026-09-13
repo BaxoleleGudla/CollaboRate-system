0cbc15356582ad387d98ccee1ce67739ed87a954
BaxoleleGudla <baxolelegudla@gmail.com>
2026-09-13 20:10:50 +0200

    Implemented user settings on API

--- Server/CollaboRateAPIServer/CollaboRateAPIServer/obj/Debug/net8.0/CollaboRateAPIServer.AssemblyInfo.cs
+++ Server/CollaboRateAPIServer/CollaboRateAPIServer/obj/Debug/net8.0/CollaboRateAPIServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c032392f083928205a46d84992c1c8495b8d28fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

edb63ed5d70087ae4e4595790582c3f68629cf04
BaxoleleGudla <baxolelegudla@gmail.com>
2026-09-17 19:37:34 +0200

    Added server method to count unread notifications

--- Server/CollaboRateAPIServer/CollaboRateAPIServer/obj/Release/net8.0/CollaboRateAPIServer.AssemblyInfo.cs
+++ Server/CollaboRateAPIServer/CollaboRateAPIServer/obj/Release/net8.0/CollaboRateAPIServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7db32c4a88cbbbb9bbdbedf3113640dfb524f1c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7406be1c82c9c379ab0219785da143c94138a03d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

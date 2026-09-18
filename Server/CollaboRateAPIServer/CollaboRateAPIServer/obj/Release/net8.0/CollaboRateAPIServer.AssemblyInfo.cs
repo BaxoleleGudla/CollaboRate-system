60133e509a7aebedb788b729f637651965b1a15e
BaxoleleGudla <baxolelegudla@gmail.com>
2026-09-19 01:37:13 +0200

    Modified AuthController

--- Server/CollaboRateAPIServer/CollaboRateAPIServer/obj/Release/net8.0/CollaboRateAPIServer.AssemblyInfo.cs
+++ Server/CollaboRateAPIServer/CollaboRateAPIServer/obj/Release/net8.0/CollaboRateAPIServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf287596b6536003d314c0f80c24300c5c60949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc52e37186203d14c5282a6797bc598fd3f05571")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

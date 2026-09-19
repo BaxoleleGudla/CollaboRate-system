203ea8a1434e76e9bb636ee695608656ee29cbeb
BaxoleleGudla <baxolelegudla@gmail.com>
2026-09-19 17:38:01 +0200

    Modified controllers

--- Server/CollaboRateAPIServer/CollaboRateAPIServer/obj/Debug/net8.0/CollaboRateAPIServer.AssemblyInfo.cs
+++ Server/CollaboRateAPIServer/CollaboRateAPIServer/obj/Debug/net8.0/CollaboRateAPIServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe566c16bb8788c6da113300c3eddfc5114cbba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollaboRateAPIServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
